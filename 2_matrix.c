c0b7b090f1441546655ad15d87f413bf341f6c17
Klepikov Dmitriy <thebrootos@gmail.com>
2022-05-07 01:06:45 +0500

    add 2 task not ready on 07.05.2022

--- 2_matrix.c
+++ 2_matrix.c
@@ -1,7 +1,11 @@
-// in work
-#include <stdin.h>
-#include <conio.h>
+/*
+The second task. The Matrix has you...
+Fill the terminal screen with random falling numbers from above,
+as in The Matrix (we can use the standard color).
+ */
+#include <stdio.h>
 
-void main () {
-    return 0
+int main (void) {
+    printf("go... 7.05.2022\n");
+    return 0;
 }
